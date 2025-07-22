55a40e960596233148d050ea225a1d36dcc05d20
ialvincom <ialvincom@ialvincomdeMacBook-Pro.local>
2025-07-22 16:14:03 +0900

    initial commit

---
+++ target/debug/deps/bibao-2b4f7763cec972b0.d
@@ -0,0 +1,5 @@
+/Users/ialvincom/Desktop/test_cursor/bibao/target/debug/deps/bibao-2b4f7763cec972b0.d: src/main.rs
+
+/Users/ialvincom/Desktop/test_cursor/bibao/target/debug/deps/libbibao-2b4f7763cec972b0.rmeta: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/bibao-bd55effcae3fd997.d
@@ -0,0 +1,5 @@
+/Users/ialvincom/Desktop/test_cursor/bibao/target/debug/deps/bibao-bd55effcae3fd997.d: src/main.rs
+
+/Users/ialvincom/Desktop/test_cursor/bibao/target/debug/deps/libbibao-bd55effcae3fd997.rmeta: src/main.rs
+
+src/main.rs:
